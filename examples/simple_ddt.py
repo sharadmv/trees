ac11c7bc05e93b17ef195c15b2704378a9eb34d6
Sharad Vikram <sharad.vikram@gmail.com>
2015-10-02 17:49:56 -0700

    inference example

--- examples/simple_ddt.py
+++ examples/simple_ddt.py
@@ -11,13 +11,9 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    X = np.matrix([
-        [0, 0],
-        [0, 1],
-        [1, 1]
-    ]).astype(np.float32)
+    D = 2
+    X = np.random.multivariate_normal(mean=np.zeros(D), cov=np.eye(D), size=100).astype(np.float32)
     N, D = X.shape
-    y = ['wat', 'who', 'where']
     df = Inverse(c=1)
     lm = GaussianLikelihoodModel(sigma=np.eye(D), mu0=np.zeros(D), sigma0=np.eye(D))
     ddt = DirichletDiffusionTree(df=df,
@@ -25,11 +21,5 @@ if __name__ == "__main__":
     mh = MetropolisHastingsSampler(ddt, X)
     mh.initialize_assignments()
 
-    # plot_tree_2d(mh.tree, X)
-    # plt.show()
-
-    # for _ in tqdm(xrange(10000)):
-        # mh.sample()
-
-    # plot_tree_2d(mh.tree, X)
-    # plt.show()
+    for _ in tqdm(xrange(10000)):
+        mh.sample()
